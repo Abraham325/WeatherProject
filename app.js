df80ae0f18c23b688ac815656244902c072392e1
Abraham325 <jamesis325@gmail.com>
2022-09-14 12:10:38 +0100

    Render the app with Live API Data

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
 const express = require("express");
 const https =  require("https");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
-app.get("/", function(req, res){
+app.use(bodyParser.urlencoded({extended: true}));
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Lagos,Nigeria&appid=37a923e5849b45934fe4926b38b2c8ec&units=metric"
+ 
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+
+
+});
+
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "37a923e5849b45934fe4926b38b2c8ec";
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
     https.get(url, function(response){
         console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in " + query +  " is " + temp + " degrees Celcius.</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send(); 
+        })
     });
 
+})
 
+  
 
-
-
-    res.send("server is up and running.");
-
-});
  
-
-
-
 
 app.listen(3000, function(){
 
